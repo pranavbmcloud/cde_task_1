cc89d51f5ea97c789b034b0f87868060d021fac0
Pranav <pranavbmcloud@gmail.com>
2020-12-21 13:53:45 +0530

    Adds some test cases

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -2,8 +2,19 @@
 
 
 import unittest
+from clean import cleaners, json_clean, xml_clean, text_clean
+from file_type import DataType
 
 
 class FileTypeTest(unittest.TestCase):
     """Tests methods in clean.py module"""
+
+    def test_cleaners_DataType_JSON(self):
+        self.assertEqual(cleaners[DataType.JSON], json_clean)
+
+    def test_cleaners_DataType_XML(self):
+        self.assertEqual(cleaners[DataType.XML], xml_clean)
+
+    def test_cleaners_DataType_TEXT(self):
+        self.assertEqual(cleaners[DataType.TEXT], text_clean)
 
--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -9,6 +9,10 @@ from source_type import DataSource
 class FileTypeTest(unittest.TestCase):
     """Tests methods in ingest.py module"""
 
+    def setUp(self) -> None:
+        self.test_file_path_for_ingest = './tests/test_files/ingest_test.txt'
+        self.data_in_ingest_test_file = ['This is a file\n', 'With some text\n', 'To be used\n', 'for testing']
+
     def test_ingesters_DataSource_local(self):
         self.assertEqual(ingesters[DataSource.LOCAL], local_ingest)
 
@@ -16,5 +20,5 @@ class FileTypeTest(unittest.TestCase):
         self.assertEqual(ingesters[DataSource.URL], url_ingest)
 
     def test_local_ingest(self):
-        self.assertEqual(local_ingest('./tests/ingest_test.txt'), ['This is a file\n', 'With some text\n', 'To be used\n', 'for testing'])
+        self.assertEqual(local_ingest(self.test_file_path_for_ingest), self.data_in_ingest_test_file)
 
